5ec410dd1001ce8b84be31dc1c7443490432d654
Y.Arimitsu <yoshikazu_arimitsu@albert2005.co.jp>
2020-01-16 13:04:46 +0900

    release 0.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,6 @@ All notable changes to the "vscode-charcode-ext" extension will be documented in
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
-## [Unreleased]
+## [0.0.1] - 2020-01-16
 
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,88 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as encjap from "encoding-japanese";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-charcode-ext" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+enum EncodingEnum {
+	UNICODE = 'UNICODE',
+	UTF8 = 'UTF8',
+	SJIS = 'SJIS',
+	EUCJP = 'EUCJP'
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+let myStatusBarItem: vscode.StatusBarItem;
+let encoding = EncodingEnum.UNICODE;
+
+export async function showQuickPick() {
+	const keys = Object.keys(EncodingEnum);
+	const result = await vscode.window.showQuickPick(keys, {
+		placeHolder: 'select encoding charset'
+	});
+	console.log(`select charset ${result}`);
+	encoding = <EncodingEnum>(result);
+}
+
+export function activate({subscriptions}: vscode.ExtensionContext) {
+	const myCommandId = 'charcode.selectEncoding';
+	subscriptions.push(vscode.commands.registerCommand(myCommandId, async () => {
+		await showQuickPick();
+		updateStatusBarItem();
+	}));
+
+	// create a new status bar item that we can now manage
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	myStatusBarItem.command = myCommandId;
+	subscriptions.push(myStatusBarItem);
+
+	// register some listener that make sure the status bar 
+	// item always up-to-date
+	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+	subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
+
+	// update status bar item once at start
+	updateStatusBarItem();
+}
+
+function updateStatusBarItem(): void {
+	const s = getCaretText(vscode.window.activeTextEditor);
+	const message = buildStatusText(s);
+
+	if (message.length > 0) {
+		myStatusBarItem.text = message;
+		myStatusBarItem.show();
+	} else {
+		myStatusBarItem.hide();
+	}
+}
+
+function getCaretText(editor: vscode.TextEditor | undefined): string {
+	if (editor) {
+		let cursorPosition = editor.selection.start;
+		let nextPosisiton = new vscode.Position(cursorPosition.line, cursorPosition.character + 1);
+		let r = new vscode.Range(cursorPosition, nextPosisiton);
+		return editor.document.getText(r);
+	}
+	return "";
+}
+
+function buildStatusText(text: string): string {
+	if(text.length) {
+		const cc = text.charCodeAt(0);
+		const encarray = encjap.convert(text, {
+			to: encoding,
+			from: 'UNICODE',
+			type: 'array'
+		  }) as number[];
+		
+		let enc = encarray.map((c:number) => {
+			return c.toString(16).toUpperCase().padStart(2, '0');
+		}).join('');
+
+		console.log(`caret len=${text.length} char=${text} U+${cc.toString(16)}, enc=${enc}`);
+		const ud = (encoding == EncodingEnum.UNICODE || encoding == EncodingEnum.UTF8) ?
+			'U+' : '';
+		return `${encoding}: ${ud}${enc}`
+	} else {
+		console.log(`caret len=${text.length} char=${text}`);
+	}
+	return '';
+}
